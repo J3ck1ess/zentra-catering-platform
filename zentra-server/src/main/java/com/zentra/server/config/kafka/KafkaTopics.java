309f8557ea57373ba5268dab4a535479aa09a2e3
Agjan <248143261+J3ck1ess@users.noreply.github.com>
2026-09-06 13:51:10 +0500

    feat: add Kafka producer and consumer infrastructure

---
+++ zentra-server/src/main/java/com/zentra/server/config/kafka/KafkaTopics.java
@@ -0,0 +1,9 @@
+package com.zentra.server.config.kafka;
+
+public final class KafkaTopics {
+
+    private KafkaTopics() {
+    }
+
+    public static final String ORDER_CREATED = "zentra.order.created";
+}
